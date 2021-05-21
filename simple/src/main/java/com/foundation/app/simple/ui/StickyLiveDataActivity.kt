5ad94e02dc2c7fdcc344bf7420c0ad330d8fc4a6
zhusw <sink50@foxmail.com>
2021-05-21 15:52:16 +0800

    opt:测试代码

--- simple/src/main/java/com/foundation/app/simple/ui/StickyLiveDataActivity.kt
+++ simple/src/main/java/com/foundation/app/simple/ui/StickyLiveDataActivity.kt
@@ -1,13 +1,11 @@
 package com.foundation.app.simple.ui
 
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import androidx.viewbinding.ViewBinding
 import com.foundation.app.arc.utils.ext.observerStickyLess
 import com.foundation.app.simple.architecture.BaseActivity
 import com.foundation.app.simple.databinding.ActStickyBinding
 import com.foundation.app.simple.vm.AppVM
-import com.foundation.app.simple.vm.ReflectionTest
 
 /**
  * 粘性测试
@@ -20,24 +18,15 @@ class StickyLiveDataActivity : BaseActivity() {
     override fun getContentVB(): ViewBinding = binding
 
     private var stickyCount = 0
-    private var stickyLessCount = 0
     override fun init(savedInstanceState: Bundle?) {
         binding.btnSticky.setOnClickListener {
             vm.data.value = stickyCount + 1
         }
 
         binding.btnStickyLess.setOnClickListener {
-            vm.data.value = stickyLessCount + 1
-            bindData()
-            ReflectionTest.test()
+            bindData()//重新订阅，测试是否被重复订阅
         }
-    }
 
-    val obs = object : Observer<Int> {
-        override fun onChanged(t: Int) {
-            stickyLessCount = t
-            binding.tv2.text = "$stickyLessCount"
-        }
     }
 
     override fun bindData() {
@@ -45,7 +34,10 @@ class StickyLiveDataActivity : BaseActivity() {
             stickyCount = it
             binding.tv.text = "$stickyCount"
         }
-        vm.data.observerStickyLess(this, obs)
+
+        vm.data.observerStickyLess(this) {
+            binding.tv2.text = "$it"
+        }
     }
 
 }
